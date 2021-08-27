39876f693ba4870ae4ead908b159c00136147b05
Matthew G <46137770+xMdb@users.noreply.github.com>
2021-08-27 12:45:47 +1000

    🎉 Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# craft - Design mockup
+
+craft is a website for a fake programming language that I came up with. It doesn't exist.
+
+### Design
+I designed this in Figma, and built it using TailwindCSS.
+
+### View
+You can view the website [here](https://xmdb.github.io/craft).
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+   purge: ['./dist/**/*.html'],
+   darkMode: false, // or 'media' or 'class'
+   theme: {
+      extend: {
+         fontFamily: {
+            sans: ['Ginger Regular'],
+         },
+      },
+   },
+   variants: {
+      extend: {},
+   },
+   plugins: [],
+};
